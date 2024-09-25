1b3d275bbd5aa40511e6988288d7fa5be9d99326
Ayoub Mazouz <amazouz@accolade.co.ma>
2024-09-25 14:15:44 +0100

    Ajoutez des fichiers projet.

---
+++ Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿using formation.Models;
+
+namespace formation.Interfaces
+{
+    public interface IUser
+    {
+        public User Save(User user);
+        public User FindBy(int id);
+        public User List();
+        public User Delete(int id);
+    }
+}
